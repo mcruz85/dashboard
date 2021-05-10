61eb4f31b41c7ceb4c40737aa5f045bb36725347
Marcus Cruz <marcus.cruz85@gmail.com>
2021-05-10 00:07:53 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+
+import Pagination from "components/Pagination";
 import { SalePage } from "types/sales";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 const DataTable = () => {
+
+  const [activePage, setActivePage] = useState(0);
   const [page, setPage] = useState<SalePage>({
     first: true,
     last: true,
@@ -14,12 +18,20 @@ const DataTable = () => {
   });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales?size=20&page=5&sort=date,desc`).then((result) => {
-      setPage(result.data);
+    axios.get(`${BASE_URL}/sales?size=10&page=${activePage}&sort=date,desc`)
+        .then((result) => {
+            setPage(result.data);
     });
-  }, []);
+  }, [activePage]);
+
+
+  const changePage = (index: number) => {
+      setActivePage(index);
+  }
 
   return (
+    <>
+     <Pagination page={page} onPageChange={changePage} />
     <div className="table-responsive">
       <table className="table table-striped table-sm">
         <thead>
@@ -33,7 +45,7 @@ const DataTable = () => {
         </thead>
         <tbody>
           {page.content?.map(item => (
-              <tr key="{item.id}">
+              <tr key={item.id}>
                 <td>{formatLocalDate(item.date, 'dd/MM/yyyy')}</td>
                 <td>{item.seller.name}</td>
                 <td>{item.visited}</td>
@@ -44,6 +56,8 @@ const DataTable = () => {
         </tbody>
       </table>
     </div>
+   
+    </>
   );
 };
 
